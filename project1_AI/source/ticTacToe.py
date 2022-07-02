818602d24539d4f18eef105872583c1ebc741b9b
Nguyen Van Viet <Nvviet17.4@gmail.com>
2022-07-02 10:59:48 +0700

    clear some test program

--- project1_AI/source/ticTacToe.py
+++ project1_AI/source/ticTacToe.py
@@ -1,188 +1,107 @@
-import time
-import numpy as np
+from multiprocessing.connection import wait
+import pygame , sys
+import alphabeta as ab
+from time import sleep
+# initializes pygame
+pygame.init()
 
-class TicTacToe:
-    def __init__(self, size):
-        self.size = size
-        self.map = np.array([["-" for _ in range(self.size)] for _ in range(self.size)])
-        self.player_turn = 'X'
+#Define color and size
+SIZE = 600 # size of the screen size x size
+BLACK = (0, 0, 0) # use for coloring line
+WHITE = (255, 255, 255) # use for coloring O
+RED = (255, 51, 51) # use for coloring X
+PURPLE = (76, 0, 153) # use for message
 
-    def draw_board(self):
-        for i in range(0, 3):
-            for j in range(0, 3):
-                print('{}|'.format(self.map[i][j]), end=" ")
-            print()
-        print()
-        
-    def is_valid(self, x, y):
-        if x < self.map.shape[0]:
-            if y < self.map.shape[1]:
-                if self.map[x][y] == "X" or self.map[x][y] == "O":
-                    return 0
-            else:
-                return 0
-        else:
-            return 0
-        return 1
+LINE_SIZE = 10
+BG_COLOR = (0,250,154) #green
 
-    def is_full(self):
-                # Is whole board full?
-        for i in range(self.size):
-            for j in range(self.size):
-                # There's an empty field, we continue the game
-                if (self.map[i][j] == "-"):
-                    return 0
-        return 1
-                
-    def is_end(self):
-        
 
-                
-        # Vertical win
-        for i in range(0, 3):
-            if (all(x == "X" for x in self.map[:,i])):
-                    return 'X'
-            elif (all(x == "O" for x in self.map[:,i])):
-                return 'O'
 
-        # Horizontal win
-        for i in range(self.size):
-            if (all(x == "X" for x in self.map[i])):
-                return 'X'
-            elif (all(x == "O" for x in self.map[i])):
-                return 'O'
+SPACE = 55
+# ------
+# SCREEN
+# ------
+screen = pygame.display.set_mode( (SIZE, SIZE) )
+pygame.display.set_caption( 'SUPER MEAG ULTRA ULTIMATE TIC TAC TOE' )
+screen.fill( BG_COLOR )
 
-        # Main diagonal win
-        for i in range(self.size - 1):
-            if (self.map[i][i] != self.map[i+1][i + 1]): #
-                return "draw"
-            return self.map[i][i]
-                
+def make_lines(n_lines):
+    pygame.draw.line( screen, BLACK, (0, 0), (SIZE, 0), LINE_SIZE )
+    pygame.draw.line( screen, BLACK, (0, 0), (0, SIZE), LINE_SIZE )
+    pygame.draw.line( screen, BLACK, (SIZE, 0), (SIZE, SIZE), LINE_SIZE )
+    pygame.draw.line( screen, BLACK, (0, SIZE), (SIZE, SIZE), LINE_SIZE )
+    
+    distance = SIZE/n_lines
+    for i in range(1, n_lines):
+        #column
+        pygame.draw.line( screen, BLACK, (distance * i, 0), (distance * i, SIZE), LINE_SIZE )
+        #row
+        pygame.draw.line( screen, BLACK, (0, distance * i), (SIZE, distance * i), LINE_SIZE )
 
-        # Second diagonal win
-        for i in range(self.size - 1):
-            #0, 3-0-1=2 <=> 0 + 1 = 1, 3 - 0 - 1 = 2
-            if (self.map[i][self.size - i - 1] != self.map[i + 1][self.size - i - 2]): #
-                return "draw"
-            return self.map[i][self.size - i - 1]
+def draw_figures(map, n_lines):
+    
+    square_size = SIZE/n_lines
+    for row in range(map.shape[0]):
+        for col in range(map.shape[0]):
+            if map[row][col] == "O":
+                pygame.draw.circle( screen, WHITE, (int( col * square_size + square_size//2 ), int( row * square_size + square_size//2 )), 180/n_lines, LINE_SIZE )
+            elif map[row][col] == "X":
+                pygame.draw.line( screen, RED, (col * square_size + SPACE, row * square_size + square_size - SPACE), (col * square_size + square_size - SPACE, row * square_size + SPACE), LINE_SIZE )	
+                pygame.draw.line( screen, RED, (col * square_size + SPACE, row * square_size + SPACE), (col * square_size + square_size - SPACE, row * square_size + square_size - SPACE), LINE_SIZE )
 
+def catch_mouse(event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        mouseX = event.pos[0] # x
+        mouseY = event.pos[1] # y
+
+        clicked_row = int(mouseY // (SIZE/3))
+        clicked_col = int(mouseX // (SIZE/3))
+        return clicked_row, clicked_col
+    return None, None
+
+ 
+
+def game_end_message(win):
+    pygame.font.init()
+    
+    if ( win == "X"):
+        text = "You win !"
+    elif (win == "O"):
+        text = "BOT win !"
+    else:
+        text = "--DRAW--"
+    my_font = pygame.font.SysFont('Comic Sans MS', 50)
+    text_surface = my_font.render(text, False, PURPLE)
+    screen.blit(text_surface, (SIZE/2 - 100,SIZE/2 - 50))
+
+#main
+mapT = ab.TicTacToe(3)
+alphaBeta = ab.AlphaBeta(mapT)
+make_lines(3)
+game = True
+
+
+while game:
+    for event in pygame.event.get():
+         
+        if event.type == pygame.QUIT:
+    	    game = False
+         
        
+        win = mapT.is_end()
+        if win != None:
+            mapT.draw_board()
+            game_end_message(win)
+            #game = False
+        else:
+            x, y = catch_mouse(event)
+            if x != None:
+                if mapT.is_valid(x, y):
+                    mapT.set(x, y, "X")
+                    bot = alphaBeta.alpha_beta_search(mapT, "O")
+                    if bot != None:
+                        mapT = bot
+                    draw_figures(mapT.map, 3)
 
-        # It's a tie!
-        return None
-
-    def max_alpha_beta(self, alpha, beta):
-            maxv = -2
-            px = None
-            py = None
-
-            result = self.is_end()
-
-            if result == 'X':
-                return (-1, 0, 0)
-            elif result == 'O':
-                return (1, 0, 0)
-            elif result == "-":
-                return (0, 0, 0)
-
-            for i in range(0, 3):
-                for j in range(0, 3):
-                    if self.map[i][j] == "-":
-                        self.map[i][j] = 'O'
-                        (m, min_i, in_j) = self.min_alpha_beta(alpha, beta)
-                        if m > maxv:
-                            maxv = m
-                            px = i
-                            py = j
-                        self.map[i][j] = "-"
-
-                        # Next two ifs in Max and Min are the only difference between regular algorithm and minimax
-                        if maxv >= beta:
-                            return (maxv, px, py)
-
-                        if maxv > alpha:
-                            alpha = maxv
-
-            return (maxv, px, py)
-        
-    def min_alpha_beta(self, alpha, beta):
-        
-            minv = 2
-
-            qx = None
-            qy = None
-
-            result = self.is_end()
-
-            if result == 'X':
-                return (-1, 0, 0)
-            elif result == 'O':
-                return (1, 0, 0)
-            elif result == "-":
-                return (0, 0, 0)
-
-            for i in range(0, 3):
-                for j in range(0, 3):
-                    if self.map[i][j] == "-":
-                        self.map[i][j] = 'X' 
-                        (m, max_i, max_j) = self.max_alpha_beta(alpha, beta)
-                        if m < minv:
-                            minv = m
-                            qx = i
-                            qy = j
-                        self.map[i][j] = "-"
-
-                        if minv <= alpha:
-                            return (minv, qx, qy)
-
-                        if minv < beta:
-                            beta = minv
-
-            return (minv, qx, qy)
-    def play_alpha_beta(self):
-        while True:
-            self.draw_board()
-            self.result = self.is_end()
-
-     
-            if self.result == 'X':
-                print('The winner is X!')
-                break
-            elif self.result == 'O':
-                print('The winner is O!')
-                break
-
-
-            if self.player_turn == 'X':
-
-                while True:
-                    start = time.time()
-                    (m, qx, qy) = self.min_alpha_beta(-2, 2)
-                    end = time.time()
-                    print('Evaluation time: {}s'.format(round(end - start, 7)))
-                    print('Recommended move: X = {}, Y = {}'.format(qx, qy))
-
-                    px = int(input('Insert the X coordinate: '))
-                    py = int(input('Insert the Y coordinate: '))
-
-                    qx = px
-                    qy = py
-
-                    if self.is_valid(px, py):
-                        self.map[px][py] = 'X'
-                        self.player_turn = 'O'
-                        break
-                    else:
-                        print('The move is not valid! Try again.')
-
-            else:
-                (m, px, py) = self.max_alpha_beta(-2, 2)
-                self.map[px][py] = 'O'
-                self.player_turn = 'X'
-                
-def main():
-    g = TicTacToe(3)
-    g.play_alpha_beta()
-
-if __name__ == "__main__":
-    main()
+    
+    pygame.display.update()
